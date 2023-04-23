313edf07857b4ced2c09c5d5ef6cc0fb2555a03c
KeepinItRail <dewsoul@pm.me>
2023-04-23 20:56:10 +0100

    fix: hirata

--- src/houses/apartments/hirata/hirata.py
+++ src/houses/apartments/hirata/hirata.py
@@ -13,9 +13,10 @@ coloursdict = {
 "light_blue":	"8",
 "dark_blue":	"9",
 "light_green":	"10",
-"black":		"11"
+"black":		"11",
+"gold":			"12",
+"red_brown":	"13"
 }
-
 building_name = "hirata"
 colours = ["white", "grey", "mauve", "brown1", "brown2", "dark_green", "peach", "pink", "light_blue", "dark_blue"]
 levels = ["3L", "4L", "5L", "6L"]
@@ -29,7 +30,7 @@ variants = {
 		"yoffset" : "0"
 	}
   }
-
+switches_required = True
 variant_names = list(variants.keys())
 
 # 1) Create the colours		
@@ -42,7 +43,7 @@ variant_names = list(variants.keys())
 
 n = 0
 for i in colours:
-	template = open("./template.pnml", "rt")
+	template = open("./colours/colour_template.pnml", "rt")
 	current_colour = open("./colours/" + colours[n] +".pnml", "wt")
 	for line in template:
 		current_colour.write(line.replace('_clr_', str('_' + colours[n] +'_')))
@@ -145,3 +146,120 @@ for i in levels:
 	n = n+1
 
 print(building_name + " spritesets created")
+
+# 1) CREATE THE DIRECTION / VARIANTS
+
+n = 0
+for i in variants:
+	switch_template = open("./switches/template_rs.pnml", "rt")
+	current_variant = open("./switches/" + variant_names[n] +".pnml", "wt")
+	for line in switch_template:
+		current_variant.write(line.replace('_v_', str('_' + variant_names[n] + '_')))
+	current_variant.close()
+	switch_template.close()
+	n = n+1
+
+# 2) MERGE THE DIRECTION / VARIANTS
+
+# File with combined output
+variants_pnml_path = "./switches/combined_variants.pnml"
+
+# Create an empty list where all the PNML code will be placed
+sections = []
+
+# Function for copying code from .pnml files
+def append_variants(file):
+    filename = "./switches/{}.pnml"
+    stuff = codecs.open(filename.format(file),'r','utf8')
+    sections.append(stuff.read())
+    stuff.close()
+
+# Append header stuff which should always be first
+for i in variants:
+    append_variants(i)
+
+# Write the content of 'sections' into a file and save it
+processed_pnml_file = codecs.open(variants_pnml_path,'w','utf8')
+processed_pnml_file.write('\n'.join(sections))
+processed_pnml_file.close()
+
+# 3) CREATE THE LEVELS
+
+n = 0
+for i in levels:
+	level_template = open("./switches/combined_variants.pnml", "rt")
+	current_level = open("./switches/" + levels[n] +"_rs.pnml", "wt")
+	for line in level_template:
+		current_level.write(line.replace('_xL_', str('_'+ levels[n] +'_')))
+	current_level.close()
+	level_template.close()
+	n = n+1
+
+# 4) MERGE THE LEVELS
+
+# File with combined output
+variants_pnml_path = "./switches/combined_rs.pnml"
+
+# Create an empty list where all the PNML code will be placed
+sections = []
+
+# Function for copying code from .pnml files
+def append_variants(file):
+    filename = "./switches/{}_rs.pnml"
+    stuff = codecs.open(filename.format(file),'r','utf8')
+    sections.append(stuff.read())
+    stuff.close()
+
+# Append header stuff which should always be first
+for i in levels:
+    append_variants(i)
+
+# Write the content of 'sections' into a file and save it
+processed_pnml_file = codecs.open(variants_pnml_path,'w','utf8')
+processed_pnml_file.write('\n'.join(sections))
+processed_pnml_file.close()
+
+print(building_name + " random switches created")
+
+if switches_required == True:
+
+	# 1) CREATE THE LEVELS
+
+	n = 0
+	for i in levels:
+		level_template = open("./switches/template_sw.pnml", "rt")
+		current_level = open("./switches/" + levels[n] +"_sw.pnml", "wt")
+		for line in level_template:
+			current_level.write(line.replace('_xL_', str('_'+ levels[n] +'_')))
+		current_level.close()
+		level_template.close()
+		n = n+1
+
+	# 4) MERGE THE LEVELS
+
+	# File with combined output
+	variants_pnml_path = "./switches/combined_sw.pnml"
+
+	# Create an empty list where all the PNML code will be placed
+	sections = []
+
+	# Function for copying code from .pnml files
+	def append_variants(file):
+		filename = "./switches/{}_sw.pnml"
+		stuff = codecs.open(filename.format(file),'r','utf8')
+		sections.append(stuff.read())
+		stuff.close()
+
+	# Append header stuff which should always be first
+	for i in levels:
+		append_variants(i)
+
+	# Write the content of 'sections' into a file and save it
+	processed_pnml_file = codecs.open(variants_pnml_path,'w','utf8')
+	processed_pnml_file.write('\n'.join(sections))
+	processed_pnml_file.close()
+
+	print(building_name + " switches created")
+
+else:
+	print("No " + building_name + " Switches Required")
