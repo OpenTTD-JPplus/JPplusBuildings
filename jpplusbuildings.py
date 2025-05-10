01c93da0640453a94fa4f701e952004f8be45d54
KeepinItRail <dewsoul@pm.me>
2025-05-10 14:45:23 +0100

    recode: ybuilding & old code purge

--- jpplusbuildings.py
+++ jpplusbuildings.py
@@ -1,50 +1,12 @@
 
-from lib import functions, buildingsjson
-
-# This file creates the majority of building related pnml files, the exception being graphics ('gfx') pnml files
-# It does not create centalised pnml files e.g. header.pnml  or src/functions files
-# The purpose behind it was to be able to update building 'stats' and colour choices easily
+from lib import buildingsjson
 
 print("Running jpplusbuildings.py")
 
-print("\tCreating JSONs")
-functions.CreateRemapJSON()
-functions.CreateBuildingPalettes()
-# New Function
+print("\tCreating Buildings JSON")
 buildingsjson.CreateBuildingsJSON()
-# Old Function
-functions.CreateItemJSON()
-# Check if buildings in spreadsheet that shoyld have a recolour palette do
-functions.CheckColourWeightingPresent()
 
 from lib import buildingsfiles
 
 print("\tRunning CreateBuildingFiles")
 buildingsfiles.CreateBuildingFiles()
-
-print("\tRunning CreateRecolourPnml")
-functions.CreateRecolourPnml()
-
-print("\tRunning CreateColourFiles")
-functions.CreateColourFiles()
-
-print("\tRunning CreateVariantFiles")
-functions.CreateVariantFiles()
-
-print("\tRunning CreateLevelsFiles")
-functions.CreateLevelsFiles()
-
-print("\tRunning CreateColourSwitches")
-functions.CreateColourSwitches()
-
-print("\tRunning CreateDirectionSwitches")
-functions.CreateDirectionSwitches()
-
-'''print("\tRunning CreateNameSwitches")
-functions.CreateNameSwitches()'''
-
-print("\tRunning PnmlCombiner")
-functions.PnmlCombiner()
-
-print("\tRunning CreateItems")
-functions.CreateItems()
