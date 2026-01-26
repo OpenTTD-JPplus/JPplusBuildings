87b8c93469683b0eed7ac1191e75a54caafcc335
KeepinItRail <dewsoul@pm.me>
2026-01-26 06:41:36 +0000

    shiro revamp

--- pybuild.sh
+++ pybuild.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python jpplusbuildings.py
+#python jpplusbuildings.py
 python nml_patcher.py -f "JPplusBuildings.pnml" -o "JPplusBuildings.nml" -b 1 -v 1
 nmlc JPplusBuildings.nml -o JPplusBuildings.grf -c -t src/custom_tags.txt -l src/lang
 python move_grf.py
